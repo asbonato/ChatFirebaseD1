44434f8d1b854af0f2c36c31cc3d433587cf46ab
asbonato <asbonato@gmail.com>
2021-05-11 22:18:06 -0300

    Initial commit

---
+++ app/src/main/java/br/saojudas/sdm21_1/chatcomfirebased1/ChatActivity.java
@@ -0,0 +1,18 @@
+package br.saojudas.sdm21_1.chatcomfirebased1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    public void enviarMensagem(View view) {
+    }
+}
